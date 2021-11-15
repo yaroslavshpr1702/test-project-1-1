ec6f1357a621f86d0ca1d6ad883703707a007349
Just_Larry <ryu2000.00@gmail.com>
2021-11-15 15:26:05 +0300

    Удаление ненужных файлов, доработка таблицы стилей Удаление неиспользуемых файлов: MainContent.js, Step...Content.js Перенос части классов в CSS файле

--- src/App.js
+++ src/App.js
@@ -223,70 +223,70 @@ class App extends Component {
         <img className="header__banner_picture" src="/images/banner_picture.png" alt="header_img"/>
       </header>
       <div className="app__content content">
-      <div className="content__main_page main_page">
-            <div className="main_page_product product">
-                <div className="product__gallery gallery">
-                    <div className="image_selector">
-                        <a><img className="img_arrows" src="/images/arrow_up.png" alt="arrow_up"/></a>
-                        <img className="img_plates" src="/images/g_img_1.png" alt="img1"/>
-                        <img className="img_plates" src="/images/g_img_2.png" alt="img2"/>
-                        <img className="img_plates" src="/images/g_img_3.png" alt="img3"/>
-                        <a><img className="img_arrows" src="/images/arrow_down.png" alt="arrow_down"/></a>
-                    </div>
-                    <img className="product_image" src="/images/g_img_1.png" alt="imgMain"/>
+        <div className="content__main_page main_page">
+          <div className="main_page_product product">
+            <div className="product__gallery gallery">
+                <div className="image_selector">
+                    <a><img className="img_arrows" src="/images/arrow_up.png" alt="arrow_up"/></a>
+                    <img className="img_plates" src="/images/g_img_1.png" alt="img1"/>
+                    <img className="img_plates" src="/images/g_img_2.png" alt="img2"/>
+                    <img className="img_plates" src="/images/g_img_3.png" alt="img3"/>
+                    <a><img className="img_arrows" src="/images/arrow_down.png" alt="arrow_down"/></a>
                 </div>
-                <div className="about_product">
-                    <div className="product_top">
-                        <h3>Best seller</h3>
-                        <div className="product_rating">
-                            <img className="rating_stars" src="/images/stars.png"/>
-                            <p>(1731 reviews)</p>
-                        </div>
+                <img className="product_image" src="/images/g_img_1.png" alt="imgMain"/>
+            </div>
+            <div className="about_product">
+                <div className="product_top">
+                    <h3>Best seller</h3>
+                    <div className="product_rating">
+                        <img className="rating_stars" src="/images/stars.png"/>
+                        <p>(1731 reviews)</p>
                     </div>
-                    <h2>Acne Treatment Cystic Acne heals, repairs & renews</h2>
-                    <h3>Description:</h3>
-                    <p className="description">Addictively refreshing gel-cream leaves skin plump, dewy, glowing.</p>
+                </div>
+                <h2>Acne Treatment Cystic Acne heals, repairs & renews</h2>
+                <h3>Description:</h3>
+                <p className="description">Addictively refreshing gel-cream leaves skin plump, dewy, glowing.</p>
 
-                    <button className="items">items left: 12</button>
+                <button className="items">items left: 12</button>
 
-                    <div className="buy_product">
-                        <div className="product__prices">
-                            <p className="price">us$25.89</p>
-                            <h2 className="sells">$0.00 FREE</h2>
-                        </div>
-                        <button className="select_button">Select</button>
-                        <img className="amazon_prime_logo" src="/images/amazon_prime_logo.png" alt="amazon_p_logo"/>
+                <div className="buy_product">
+                    <div className="product__prices">
+                        <p className="price">us$25.89</p>
+                        <h2 className="sells">$0.00 FREE</h2>
                     </div>
+                    <button className="select_button">Select</button>
+                    <img className="amazon_prime_logo" src="/images/amazon_prime_logo.png" alt="amazon_p_logo"/>
                 </div>
             </div>
-            <div className="product__overview overview">
-                <h1>Overview</h1>
-                <h3>Who It's For</h3>
-                <p>Very Dry, Dry, Dry Combination Skin (Type 1, 2)</p>
-                <h3>What It Is</h3>
-                <p>Step 3 in our customized 3-Step Skincare System. Dermatologist-developed face moisturizer softens, smooths, improves. Leaves skin glowing.</p>
-                <h3>What It Does</h3>
-                <ul>
-                    <li>Silky lotion delivers 8-hour hydration.</li>
-                    <li>Slips on easily, absorbs quickly.</li>
-                    <li>Helps strengthen skin’s own moisture barrier so more moisture stays in. Skin that holds onto moisture has a youthful-looking glow.</li>
-                </ul>
-                <a href="#" className="read_more not_toggled" onClick={(e) => {this.ReadMoreFunction(e)}}>Read more...</a>
-                <div className="hidden_text text_hide">
-                    <h3>Key Ingredients</h3>
-                    <p>Sunflower Seed Cake, Barley Extract and Cucumber Fruit Extract help strengthen skin’s barrier, improve its resiliency, and balance and retain skin’s moisture levels. Hyaluronic acid acts as a natural moisture magnet/humectant.</p>
-                    <div className="buy_product">
-                        <div className="product__prices">
-                            <p className="price">us$25.89</p>
-                            <h2 className="sells">$0.00 FREE</h2>
-                        </div>
-                        <button className="select_button">Select</button>
-                        <img className="amazon_prime_logo" src="/images/amazon_prime_logo.png" alt="amazon_p_logo"/>
-                    </div>
+          </div>
+          <div className="product__overview overview">
+            <h1>Overview</h1>
+            <h3>Who It's For</h3>
+            <p>Very Dry, Dry, Dry Combination Skin (Type 1, 2)</p>
+            <h3>What It Is</h3>
+            <p>Step 3 in our customized 3-Step Skincare System. Dermatologist-developed face moisturizer softens, smooths, improves. Leaves skin glowing.</p>
+            <h3>What It Does</h3>
+            <ul>
+              <li>Silky lotion delivers 8-hour hydration.</li>
+              <li>Slips on easily, absorbs quickly.</li>
+              <li>Helps strengthen skin’s own moisture barrier so more moisture stays in. Skin that holds onto moisture has a youthful-looking glow.</li>
+            </ul>
+            <a href="#" className="read_more not_toggled" onClick={(e) => {this.ReadMoreFunction(e)}}>Read more...</a>
+            <div className="hidden_text text_hide">
+              <h3>Key Ingredients</h3>
+              <p>Sunflower Seed Cake, Barley Extract and Cucumber Fruit Extract help strengthen skin’s barrier, improve its resiliency, and balance and retain skin’s moisture levels. Hyaluronic acid acts as a natural moisture magnet/humectant.</p>
+              <div className="buy_product">
+                <div className="product__prices">
+                  <p className="price">us$25.89</p>
+                  <h2 className="sells">$0.00 FREE</h2>
                 </div>
+                <button className="select_button">Select</button>
+                <img className="amazon_prime_logo" src="/images/amazon_prime_logo.png" alt="amazon_p_logo"/>
+              </div>
             </div>
+          </div>
         </div>
-      </div>  
+      </div>
       <div className="app__how_it_work how_it_work text_hide">
         <h1>How does it work?</h1>
         <div className="how_it_work__steps steps">
